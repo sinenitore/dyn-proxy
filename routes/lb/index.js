76aac995037eb72405d651102edeed50ca820d8c
sinenitore <sine.nitore@gmail.com>
2014-02-10 09:28:42 -0800

    LB add route pareses variables correctly.

--- routes/lb/index.js
+++ routes/lb/index.js
@@ -5,27 +5,29 @@
 
 exports.add = function(req, res){
   //check for existing
-  var data = {'name': req.name,
-	      'node': req.node,
-	      'url': req.url}
+  var data = {'name': req.params.name,
+	      'node': req.params.node,
+	      'url': req.params.url}
+  console.log(data)
   var etcd = require('etcd');
-  function setConf(cdata, callback){
-    etcd.configure({
-      host: '172.18.4.36',
-      port: 4001
-    });
+  etcd.configure({
+    host: '172.18.4.36',
+    port: 4001
+  });
+  console.log(etcd)
+//  function setConf(cdata, callback){
 //    etcd.set('/lb/instance/' + data.name + '/node', data.node, function (err) {
 //      if (err) throw err;
 //    }
 //    etcd.set('/lb/instance/' + data.name + '/url', data.node, function (err) {
 //      if (err) throw err;
 //    }
-      etcd.set('test', 'ping', function(err){
-        if (err) {
-	  res.send(err)
-        } else {
-	  res.redirect('/list')
-	}
-      });
-    }
+    etcd.set('test', 'ping', function(err){
+      if (err) {
+        res.send(err)
+      } else {
+        res.redirect('/list')
+      }
+    });
+//  }
 };
